e5d5c47076908458faa581d29f58d3d5ee81ced3
Walter Willis <waltertwillis@outlook.com>
2019-12-15 14:57:56 -0500

    Initial structure setup for mobile app.

---
+++ Rover2020MobileController/Rover2020MobileController/Rover2020MobileController/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rover2020MobileController.Services;
+using Rover2020MobileController.Views;
+
+namespace Rover2020MobileController
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Rover2020MobileController/Rover2020MobileController/Rover2020MobileController/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Rover2020MobileController.Models;
+
+namespace Rover2020MobileController.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
