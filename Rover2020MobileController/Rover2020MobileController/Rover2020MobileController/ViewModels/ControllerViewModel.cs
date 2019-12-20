cc842ecb2a139d55b1ec835c0fc4d7a2d7597c55
Walter Willis <waltertwillis@outlook.com>
2019-12-20 00:22:59 -0500

    Implement first ViewModel design pattern for slider
    Created responsive label for the slider. Now shows the speed when changing the slider.
    Design of the page has improved as well.

---
+++ Rover2020MobileController/Rover2020MobileController/Rover2020MobileController/ViewModels/ControllerViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rover2020MobileController.ViewModels
+{
+    public class ControllerViewModel : BaseViewModel
+    {
+        private int speed;
+        public int Speed { get => speed; set => SetProperty(ref speed, value); }
+        public ControllerViewModel(int speed = 100)
+        {
+            Speed = speed;
+        }
+    }
+}
--- Rover2020MobileController/Rover2020MobileController/Rover2020MobileController/Views/ControlPage.xaml.cs
+++ Rover2020MobileController/Rover2020MobileController/Rover2020MobileController/Views/ControlPage.xaml.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Rover2020MobileController.ViewModels;
 
 namespace Rover2020MobileController.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ControlPage : ContentPage
     {
+        ControllerViewModel viewModel;
         public ControlPage()
         {
             InitializeComponent();
+
+            BindingContext = viewModel = new ControllerViewModel();
         }
     }
 }
