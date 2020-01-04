e41ecb412f0bc897c68d5d18230f26f3a4d47efe
WalterWillis <waltertwillis@outlook.com>
2020-01-04 01:22:44 -0500

    Converted current code to .NET Core 3.1 WPF App
    The xamarin bluetooth situation is this: Too complicated.
    
    I'd need to get to know the way each platform handles bluetooth which is not worth the time for a simple app. Converting it to WPF instead and sticking with the windows platform API.

---
+++ Rover2020MobileController/BluetoothController/BluetoothController/ControlPage.xaml.cs
@@ -0,0 +1,91 @@
+﻿using BluetoothController.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BluetoothController
+{
+    public partial class ControlPage : Window
+    {
+        private ControllerViewModel ViewModel { get; set; }
+
+        public ControlPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new ControllerViewModel();
+            DataContext = ViewModel;
+            displayList.ItemsSource = ViewModel.DisplayVariants;
+            displayList.SelectedIndex = 0;
+
+            powerButton.Click += PowerOffDialog;
+            upButton.Click += MoveForward;
+            downButton.Click += MoveBackward;
+            leftButton.Click += MoveLeft;
+            rightButton.Click += MoveRight;
+
+            speedIncreaseButton.Click += IncreaseSpeed;
+            speedDecreaseButton.Click += DecreaseSpeed;
+        }
+        private async void PowerOffDialog(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "Are you sure you want to power down the rover?";
+            string caption = "Device Shutdown";
+
+            MessageBoxResult result = MessageBox.Show(messageBoxText, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result.Equals(MessageBoxResult.Yes))
+            {
+                await ViewModel.PowerOffDevice();
+            }
+        }
+
+        private void Move(object sender, RoutedEventArgs e)
+        {
+            var keyword = (e.Source as Button).Content.ToString();
+            Enum.TryParse(keyword, out ControllerViewModel.Direction direction);
+            MessageBox.Show(keyword);
+            ViewModel.Move(direction);
+        }
+        private void MoveForward(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Move(ControllerViewModel.Direction.FORWARD);
+        }
+
+        private void MoveBackward(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Move(ControllerViewModel.Direction.BACKWARD);
+        }
+
+        private void MoveLeft(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Move(ControllerViewModel.Direction.LEFT);
+        }
+        private void MoveRight(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Move(ControllerViewModel.Direction.RIGHT);
+        }
+
+        private void IncreaseSpeed(object sender, RoutedEventArgs e)
+        {
+            ViewModel.IncreaseSpeed();
+        }
+
+        private void DecreaseSpeed(object sender, RoutedEventArgs e)
+        {
+            ViewModel.DecreaseSpeed();
+        }
+    }
+
+}
---
+++ Rover2020MobileController/BluetoothController/BluetoothController/ControllerViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace BluetoothController.ViewModels
+{
+    public class ControllerViewModel : INotifyPropertyChanged
+    {
+        private int speed;
+        private int min;
+        private int max;
+        private int speedIncrement = 10;
+        private List<string> displayVariants;
+        private List<string> devices;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Speed { get => speed; set =>  SetProperty(ref speed, value); }
+        public List<string> DisplayVariants 
+            { get => displayVariants; set => SetProperty(ref displayVariants, value); }
+
+        public List<string> Devices
+        { get => devices; set => SetProperty(ref devices, value); }
+
+        public ControllerViewModel(int speed = 100, int minimum = 0, int maximum = 255,
+            int increments = 10)
+        {
+            Speed = speed;
+            min = minimum;
+            max = maximum;
+            speedIncrement = increments;
+
+            List<string> displays = new List<string>();
+            displays.Add("BRCTC SpaceRover UI Phase I");
+            DisplayVariants = displays;
+        }
+
+        public async Task PowerOffDevice()
+        {
+            await Task.Run(() =>
+            { System.Diagnostics.Debug.WriteLine("Powered Off!"); /*power down device*/ });
+        }
+
+        public void IncreaseSpeed()
+        {
+            if (speed != max)
+            {
+                int newSpeed = speed + speedIncrement;
+
+                if (newSpeed >= max)
+                    Speed = max;
+                else
+                    Speed = newSpeed;
+            }
+        }
+
+        public void DecreaseSpeed()
+        {
+            if (speed != min)
+            {
+                int newSpeed = speed - speedIncrement;
+
+                if (newSpeed <= min)
+                    Speed = min;
+                else
+                    Speed = newSpeed;
+            }
+        }
+
+        public void Move(Direction direction)
+        {
+            System.Diagnostics.Debug.WriteLine("Moving " + direction.ToString());
+        }
+
+        public enum Direction { NONE, FORWARD, BACKWARD, LEFT, RIGHT}
+
+    protected bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName]string propertyName = "",
+            Action onChanged = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(backingStore, value))
+            return false;
+
+        backingStore = value;
+        onChanged?.Invoke();
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    #region INotifyPropertyChanged
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        var changed = PropertyChanged;
+        if (changed == null)
+            return;
+
+        changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    #endregion
+}
+}
