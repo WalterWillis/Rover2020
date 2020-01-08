5ce03c8f9244605fdd5914dd3573bb83801a3584
WalterWillis <waltertwillis@outlook.com>
2020-01-08 16:12:32 -0500

    Update Test Client
    Overrode the need for mutual authentication.

--- GrpcGreeterClient/GrpcGreeterClient/Program.cs
+++ GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -3,6 +3,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using GrpcGreeter;
 using Grpc.Net.Client;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace GrpcGreeterClient
 {
@@ -10,12 +12,36 @@ namespace GrpcGreeterClient
     {
         static async Task Main(string[] args)
         {
-            // The port number(5001) must match the port of the gRPC server.
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            bool endProgram = false;
+
+            //We don't care about mutual authentication here, so accept any cert
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => 
+            { 
+                return true; 
+            };
+            var httpClient = new HttpClient(handler);
+            
+            //Create the channel given our parameters
+            var channel = GrpcChannel.ForAddress("https://MobileDestroyer:5443", new GrpcChannelOptions { HttpClient = httpClient });
             var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(
-                              new HelloRequest { Name = "GreeterClient", Direction = Direction.Forward });
-            Console.WriteLine("Greeting: " + reply.Message);
+            while (!endProgram)
+            {
+                try
+                {
+                    var reply = await client.SayHelloAsync(
+                                      new HelloRequest { Name = "GreeterClient", Direction = Direction.Forward });
+                    Console.WriteLine("Greeting: " + reply.Message);
+                } 
+                //errors are likely due to a mishap in configuration, so ignore for debug
+                finally
+                {
+                    var input = Console.ReadKey();
+
+                    if (input.Key == ConsoleKey.Y)
+                        endProgram = true;
+                }
+            }
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
