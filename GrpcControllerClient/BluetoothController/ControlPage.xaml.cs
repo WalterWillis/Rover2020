d270cc5e3a13192894ab574e155d3b79ef3ba08b
Walter Willis <waltertwillis@outlook.com>
2020-01-08 21:31:21 -0500

    Added UI Client
    Added MVVM style .Net Core 3.1 WPF UI app for use in our rover experiments. The address of the server is in the settings file, under the properties directory.

---
+++ GrpcControllerClient/BluetoothController/ControlPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using ControllerUI.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace ControllerUI
+{
+    public partial class ControlPage : Window
+    {
+        private ControllerViewModel ViewModel { get; set; }
+
+        public ControlPage()
+        {
+            InitializeComponent();
+
+            ViewModel = new ControllerViewModel();
+            DataContext = ViewModel;
+            displayList.ItemsSource = ViewModel.DisplayVariants;
+            displayList.SelectedIndex = 0;
+
+            powerButton.Click += PowerOffDialog;
+            upButton.Click += MoveForward;
+            downButton.Click += MoveBackward;
+            leftButton.Click += MoveLeft;
+            rightButton.Click += MoveRight;
+
+            speedIncreaseButton.Click += IncreaseSpeed;
+            speedDecreaseButton.Click += DecreaseSpeed;
+        }
+        private async void PowerOffDialog(object sender, RoutedEventArgs e)
+        {
+            string messageBoxText = "Are you sure you want to power down the rover?";
+            string caption = "Device Shutdown";
+
+            MessageBoxResult result = MessageBox.Show(messageBoxText, caption, MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result.Equals(MessageBoxResult.Yes))
+            {
+                await ViewModel.PowerOffDevice();
+            }
+        }
+
+        private void Move(object sender, RoutedEventArgs e)
+        {
+            var keyword = (e.Source as Button).Content.ToString();
+            Enum.TryParse(keyword, out ControllerViewModel.Direction direction);
+            MessageBox.Show(keyword);
+            ViewModel.Move(direction);
+        }
+        private void MoveForward(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Move(ControllerViewModel.Direction.FORWARD);
+        }
+
+        private void MoveBackward(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Move(ControllerViewModel.Direction.BACKWARD);
+        }
+
+        private void MoveLeft(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Move(ControllerViewModel.Direction.LEFT);
+        }
+        private void MoveRight(object sender, RoutedEventArgs e)
+        {
+            ViewModel.Move(ControllerViewModel.Direction.RIGHT);
+        }
+
+        private void IncreaseSpeed(object sender, RoutedEventArgs e)
+        {
+            ViewModel.IncreaseSpeed();
+        }
+
+        private void DecreaseSpeed(object sender, RoutedEventArgs e)
+        {
+            ViewModel.DecreaseSpeed();
+        }
+    }
+
+}
---
+++ GrpcControllerClient/BluetoothController/ControllerViewModel.cs
@@ -0,0 +1,131 @@
+﻿using Grpc.Net.Client;
+using GrpcController;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace ControllerUI.ViewModels
+{
+    public class ControllerViewModel : INotifyPropertyChanged
+    {
+        private int speed;
+        private int min;
+        private int max;
+        private int speedIncrement = 10;
+        private List<string> displayVariants;
+        private List<string> devices;
+        private Controller.ControllerClient client;
+        private GrpcChannel channel;
+
+        public string ConnectionStatus { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Speed { get => speed; set => SetProperty(ref speed, value); }
+        public List<string> DisplayVariants
+        { get => displayVariants; set => SetProperty(ref displayVariants, value); }
+
+        public List<string> Devices
+        { get => devices; set => SetProperty(ref devices, value); }
+
+        public ControllerViewModel(int speed = 100, int minimum = 0, int maximum = 255,
+            int increments = 10)
+        {
+            Speed = speed;
+            min = minimum;
+            max = maximum;
+            speedIncrement = increments;
+
+            List<string> displays = new List<string>();
+            displays.Add("BRCTC SpaceRover UI Phase I");
+            DisplayVariants = displays;
+
+            var handler = new HttpClientHandler();
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                return true;
+            };
+            var httpClient = new HttpClient(handler);
+
+            //Create the channel given our parameters
+            channel = GrpcChannel.ForAddress(Properties.app.Default.TargetURL, new GrpcChannelOptions { HttpClient = httpClient });
+            client = new Controller.ControllerClient(channel);
+            ConnectionStatus = "Online";
+        }
+
+        public async Task PowerOffDevice()
+        {
+            await Task.Run(() =>
+            { System.Diagnostics.Debug.WriteLine("Powered Off!"); /*power down device*/ });
+        }
+
+        public void IncreaseSpeed()
+        {
+            if (speed != max)
+            {
+                int newSpeed = speed + speedIncrement;
+
+                if (newSpeed >= max)
+                    Speed = max;
+                else
+                    Speed = newSpeed;
+            }
+        }
+
+        public void DecreaseSpeed()
+        {
+            if (speed != min)
+            {
+                int newSpeed = speed - speedIncrement;
+
+                if (newSpeed <= min)
+                    Speed = min;
+                else
+                    Speed = newSpeed;
+            }
+        }
+
+        public async void Move(Direction direction)
+        {
+            System.Diagnostics.Debug.WriteLine("Moving " + direction.ToString());
+
+            if (ConnectionStatus == "Online")
+            {
+                var reply = await client.MoveAsync(
+                    new MoveRequest { Name = "Command Center", 
+                        //Convert the UI enum to the grpc enum
+                        Direction = (GrpcController.Direction)((int)direction) });
+            }
+        }
+
+        public enum Direction { FORWARD, BACKWARD, LEFT, RIGHT }
+
+        //Found online - super useful
+        protected bool SetProperty<T>(ref T backingStore, T value,
+                [CallerMemberName]string propertyName = "",
+                Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        #region INotifyPropertyChanged
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
