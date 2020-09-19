aaa7615235aee7877ac5c60d2d6bde8a0a192612
WalterWillis <waltertwillis@outlook.com>
2020-09-19 15:52:14 -0400

    Created new Rover Mobile Application
    It's been quite a while since I last looked at the failed Rover Application. The app was in a broken state due to changes to Xamarin. Instead, I moved the Controller logic to a brand new Xamarin Forms Template.
    
    Xamarin has improved tremendously in that time and was easy to get the logic integrated. From here, all I now need to do is implement the logic for GRPC that the windows application has and see if it works.

---
+++ RoverMobile/RoverMobile/RoverMobile/Views/ControllerPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using RoverMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RoverMobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ControllerPage : ContentPage
+    {
+        ControllerViewModel viewModel;
+
+        //In case we need to modify functionality on the fly in the future
+        #region Commands        
+        private Command Forward { get; set; }
+        private Command Backward { get; set; }
+        private Command Left { get; set; }
+        private Command Right { get; set; }
+        private Command IncreaseSpeed { get; set; }
+        private Command DecreaseSpeed { get; set; }
+        #endregion
+
+        public ControllerPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ControllerViewModel();
+            displayPicker.ItemsSource = viewModel.DisplayVariants;
+
+            //Can have multiple recognizers. One tap will cause an animation, two will trigger an action.
+            var tapGestureAnimation = new TapGestureRecognizer();
+            var tapGestureTrigger = new TapGestureRecognizer();
+
+            //https://forums.xamarin.com/discussion/155965/make-the-text-of-a-label-fill-up-the-container-auto-font-size
+            tapGestureAnimation.Tapped += async (s, e) =>
+            {
+                // more info about animations:
+                // https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/animation/simple
+
+                // scale the frame to x1.2
+                var scaleUpAnimationTask = powerButtonFrame.ScaleTo(1.2, 500);
+                // set opacity to 0 (transparent)
+                var fadeOutAnimationTask = powerButtonFrame.FadeTo(0, 500);
+
+                // wait for the 2 animations to finish...
+                await Task.WhenAll(scaleUpAnimationTask, fadeOutAnimationTask);
+
+                // scale the frame back to original size
+                var scaleDownAnimationTask = powerButtonFrame.ScaleTo(1, 500);
+                // set opacity back to 1 (solid)
+                var fadeInAnimationTask = powerButtonFrame.FadeTo(1, 500);
+
+                // wait for the 2 animations to finish...
+                await Task.WhenAll(scaleDownAnimationTask, fadeInAnimationTask);
+            };
+
+            tapGestureTrigger.NumberOfTapsRequired = 2;
+            tapGestureTrigger.Command = new Command(async () => await PowerOffDialog());
+
+            powerButtonFrame.GestureRecognizers.Add(tapGestureAnimation);
+            powerButtonFrame.GestureRecognizers.Add(tapGestureTrigger);
+
+            upButton.Command = Forward = new Command(_ => viewModel.Move(ControllerViewModel.Direction.FORWARD));
+            downButton.Command = Backward = new Command(_ => viewModel.Move(ControllerViewModel.Direction.BACKWARD));
+            leftButton.Command = Left = new Command(_ => viewModel.Move(ControllerViewModel.Direction.LEFT));
+            rightButton.Command = Right = new Command(_ => viewModel.Move(ControllerViewModel.Direction.RIGHT));
+
+            speedIncreaseButton.Command = IncreaseSpeed = new Command(_ => viewModel.IncreaseSpeed());
+            speedDecreaseButton.Command = DecreaseSpeed = new Command(_ => viewModel.DecreaseSpeed());
+        }
+
+        private async Task PowerOffDialog()
+        {
+            bool result = await DisplayAlert("Device Shutdown",
+                "Are you sure you want to power down the rover?", "Yes", "No");
+
+            if (result)
+                await viewModel.PowerOffDevice();
+        }
+    }
+}
---
+++ RoverMobile/RoverMobile/RoverMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using RoverMobile.Models;
+using RoverMobile.ViewModels;
+
+namespace RoverMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
